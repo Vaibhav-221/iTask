85ec29da47fa2e1e31473bd9e0169813fd3ec24c
Vaibhav-221 <vaibhavsinghcbm@gmail.com>
2026-03-13 01:45:49 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="flex gap-2 bg-slate-600 list-none m-0 py-0">
+        <div className="">iTask</div>
+        <li>Home</li>
+        <li>Your Task</li>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
